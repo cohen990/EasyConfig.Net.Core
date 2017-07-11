1fd9ca68fffcb84139412c75a538dc0b69c1a868
Samantha Cohen <samanthacohen294@gmail.com>
2017-07-11 13:58:53 +0100

    make exceptions inherit from EasyConfigException
    allows try/catches to target only the designed exceptions thrown by easyconfig

---
+++ EasyConfig.Net.Core/Exceptions/ConfigurationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EasyConfig.Exceptions
+{
+    public class EasyConfigException : Exception
+    {
+        public EasyConfigException(string message) : base(message)
+        {
+        }
+    }
+}
--- EasyConfig.Net.Core/Exceptions/ConfigurationTypeException.cs
+++ EasyConfig.Net.Core/Exceptions/ConfigurationTypeException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace EasyConfig.Exceptions
 {
-    public class ConfigurationTypeException : Exception
+    public class ConfigurationTypeException : EasyConfigException
     {
         public ConfigurationTypeException(string configKey, Type type): base($"Configuration parameter found with '{configKey}' cannot be parsed into '{type}'")
         {
