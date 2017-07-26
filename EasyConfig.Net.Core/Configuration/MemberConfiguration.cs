8a8676cea52b4ee65f222f979858403e21fa3ad4
Samantha Cohen <samanthacohen294@gmail.com>
2017-07-26 12:50:58 +0100

    add support for overriding properties

--- EasyConfig.Net.Core/Configuration/MemberConfiguration.cs
+++ EasyConfig.Net.Core/Configuration/MemberConfiguration.cs
@@ -7,25 +7,30 @@ namespace EasyConfig.Configuration
     {
         protected MemberConfiguration(
             object defaultValue,
-            bool hasDefault,
             bool isRequired,
             bool shouldHideInLog,
             ConfigurationAttribute configurationAttribute,
+            ConfigurationSources? overrideSource,
+            string overrideKey,
             Type type)
         {
             DefaultValue = defaultValue;
-            HasDefault = hasDefault;
             IsRequired = isRequired;
             ShouldHideInLog = shouldHideInLog;
             Key = configurationAttribute.Key;
             Alias = configurationAttribute.Alias;
             ConfigurationSources = configurationAttribute.ConfigurationSources;
+            OverrideSource = overrideSource;
+            OverrideKey = overrideKey;
             MemberType = type;
         }
 
+        public ConfigurationSources? OverrideSource { get; set; }
+        public string OverrideKey { get; }
+
         public Type MemberType { get; set; }
 
-        public bool HasDefault { get; }
+        public bool HasDefault => DefaultValue != null;
         public bool IsRequired { get; }
         public bool ShouldHideInLog { get; }
         public object DefaultValue { get; }
