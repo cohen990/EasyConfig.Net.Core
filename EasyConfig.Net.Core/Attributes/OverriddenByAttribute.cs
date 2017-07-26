61b53c1d3271ab0a191a5211d974eceb7c068439
Samantha Cohen <samanthacohen294@gmail.com>
2017-07-26 15:20:06 +0100

    overriden -> overridden

--- EasyConfig.Net.Core/Attributes/OverriddenByAttribute.cs
+++ EasyConfig.Net.Core/Attributes/OverriddenByAttribute.cs
@@ -2,12 +2,12 @@ using System;
 
 namespace EasyConfig.Attributes
 {
-    public class OverridenByAttribute : Attribute
+    public class OverriddenByAttribute : Attribute
     {
         public ConfigurationSources Source { get; }
         public string AlternativeKey { get; }
 
-        public OverridenByAttribute(ConfigurationSources source, string alternativeKey = null)
+        public OverriddenByAttribute(ConfigurationSources source, string alternativeKey = null)
         {
             Source = source;
             AlternativeKey = alternativeKey;
