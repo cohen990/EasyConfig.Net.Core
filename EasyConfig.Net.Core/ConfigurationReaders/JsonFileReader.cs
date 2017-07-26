9c0dc28ae935692a65ed64401e84b9921fa33b6f
Samantha Cohen <samanthacohen294@gmail.com>
2017-07-26 14:58:58 +0100

    refactor config to multiple files

---
+++ EasyConfig.Net.Core/ConfigurationReaders/JsonFileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace EasyConfig.ConfigurationReaders
+{
+    public class JsonFileReader : IConfigurationReader
+    {
+        private readonly IConfigurationRoot _config;
+
+        public JsonFileReader(IConfigurationRoot config)
+        {
+            _config = config;
+        }
+
+        public bool TryGet(string key, string alias, ConfigurationSources sources, out string value)
+        {
+            if (sources.HasFlag(ConfigurationSources.ConfigFile))
+            {
+                try
+                {
+                    value = _config[key];
+                    return true;
+                }
+                catch
+                {
+                }
+            }
+
+            value = "";
+            return false;
+        }
+    }
+}
