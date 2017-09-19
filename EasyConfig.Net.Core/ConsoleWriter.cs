3f2690b47e5b3418725f149ecc4653dd9280c243
Samantha Cohen <samanthacohen294@gmail.com>
2017-09-19 13:35:13 +0100

    extract writer and value setters

---
+++ EasyConfig.Net.Core/ConsoleWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EasyConfig
+{
+    public class ConsoleWriter : Writer
+    {
+        public void WriteConfigurationValue(string key, string value)
+        {
+            WriteLine($"Using '{value}' for '{key}'");
+        }
+        
+        public void ObfuscateConfigurationValue(string key, string value)
+        {
+            var obfuscatedValue = new string('*', value.Length);
+
+            WriteLine($"Using '{value}' for '{key}'");
+        }
+
+        private void WriteLine(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
