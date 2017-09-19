5bcde2744d7f28572d097e9d3b74e7f0426e7917
Dan Cohen <dan.cohen@codurance.com>
2017-09-19 14:23:54 +0100

    move setters to their own namespace

--- EasyConfig.Net.Core/ValueSetters/ValueSetter.cs
+++ EasyConfig.Net.Core/ValueSetters/ValueSetter.cs
@@ -1,9 +1,24 @@
-﻿using EasyConfig.Members;
+﻿using System;
+using EasyConfig.Members;
 
 namespace EasyConfig.ValueSetters
 {
-    public interface ValueSetter
+    public abstract class ValueSetter
     {
-        void SetTo<T>(Member member, T result);
+        public abstract void SetTo<T>(Member member, T result);
+
+        public static ValueSetter GetAppropriateSetter(Member member, string value)
+        {
+            if (member.MemberType == typeof(Uri))
+                return new UriSetter(value);
+
+            if (member.MemberType == typeof(int))
+                return new IntSetter(value);
+
+            if (member.MemberType == typeof(bool))
+                return new BoolSetter(value);
+
+            return new StringSetter(value);
+        }
     }
 }
