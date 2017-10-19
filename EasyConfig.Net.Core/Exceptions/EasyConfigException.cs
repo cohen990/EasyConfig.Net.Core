5b81b6a2e654d5b39a1534527c8488befabbef7a
Samantha Cohen <samanthacohen294@gmail.com>
2017-10-19 15:19:58 +0100

    include EnumconfigParseException in documentation

--- EasyConfig.Net.Core/Exceptions/EasyConfigException.cs
+++ EasyConfig.Net.Core/Exceptions/EasyConfigException.cs
@@ -11,12 +11,4 @@ namespace EasyConfig.Exceptions
         {
         }
     }
-
-    public class EnumConfigParseException : EasyConfigException
-    {
-        public EnumConfigParseException(string failedParse, Type enumType)
-            : base($"Failed to parse an enum of type '{enumType}' from '{failedParse}'")
-        {
-        }
-    }
 }
